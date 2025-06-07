54cb091edd113b3825961b33abf826d96146daa7
m-tayyab-mughal <mtm0277126@gmail.com>
2025-06-07 21:57:57 +0500

    Update Project

--- Program.cs
+++ Program.cs
@@ -2,6 +2,71 @@
 
 class Program
 {
+    static void Main()
+    {
+        int choice = 0;
+
+        do
+        {
+            Console.WriteLine("Welcome!");
+            Console.WriteLine("1 = Sum");
+            Console.WriteLine("2 = Subtraction");
+            Console.WriteLine("3 = Multiplication");
+            Console.WriteLine("4 = Division");
+            Console.WriteLine("5 = Modulus");
+            Console.WriteLine("0 = Exit");
+            Console.WriteLine("Please select an operation by entering the corresponding number (0-5):");
+
+            choice = int.Parse(Console.ReadLine()!);
+
+            if (choice == 0)
+            {
+                Console.WriteLine("Goodbye!");
+                break; 
+            }
+
+            Console.WriteLine("Please enter 1st number:");
+            int a = int.Parse(Console.ReadLine()!);
+
+            Console.WriteLine("Please enter 2nd number:");
+            int b = int.Parse(Console.ReadLine()!);
+
+            int result = 0;
+
+            switch (choice)
+            {
+                case 1:
+                    result = ADD(a, b);
+                    Console.WriteLine($"The sum of {a} and {b} is {result}.");
+                    break;
+                case 2:
+                    result = SUB(a, b);
+                    Console.WriteLine($"The subtraction of {a} and {b} is {result}.");
+                    break;
+                case 3:
+                    result = MUL(a, b);
+                    Console.WriteLine($"The multiplication of {a} and {b} is {result}.");
+                    break;
+                case 4:
+                    result = DIV(a, b);
+                    if (b != 0)
+                        Console.WriteLine($"The division of {a} and {b} is {result}.");
+                    break;
+                case 5:
+                    result = MOD(a, b);
+                    if (b != 0)
+                        Console.WriteLine($"The modulus of {a} and {b} is {result}.");
+                    break;
+                default:
+                    Console.WriteLine("Invalid choice! Please enter a number from 0 to 5.");
+                    break;
+            }
+
+            Console.WriteLine(); // Blank line for better readability
+
+        } while (true);
+    }
+
     // Addition Function
     static int ADD(int a, int b)
     {
@@ -40,77 +105,5 @@ class Program
             return 0;
         }
         return a % b;
-    }
-
-    static void Main()
-    {
-        Console.WriteLine("Welcome!");
-        Console.WriteLine("1 = Sum");
-        Console.WriteLine("2 = Subtraction");
-        Console.WriteLine("3 = Multiplication");
-        Console.WriteLine("4 = Division");
-        Console.WriteLine("5 = Modulus");
-        Console.WriteLine("Please select an operation by entering the corresponding number (1-5):");
-
-        // Read user operation
-        int num = int.Parse(Console.ReadLine()!);
-        int a, b, result;
-
-        switch (num)
-        {
-            case 1:
-                Console.WriteLine("You selected Sum.");
-                Console.WriteLine("Enter 1st number:");
-                a = int.Parse(Console.ReadLine()!);
-                Console.WriteLine("Enter 2nd number:");
-                b = int.Parse(Console.ReadLine()!);
-                result = ADD(a, b);
-                Console.WriteLine($"The sum of {a} and {b} is {result}.");
-                break;
-
-            case 2:
-                Console.WriteLine("You selected Subtraction.");
-                Console.WriteLine("Enter 1st number:");
-                a = int.Parse(Console.ReadLine()!);
-                Console.WriteLine("Enter 2nd number:");
-                b = int.Parse(Console.ReadLine()!);
-                result = SUB(a, b);
-                Console.WriteLine($"The result of subtracting {b} from {a} is {result}.");
-                break;
-
-            case 3:
-                Console.WriteLine("You selected Multiplication.");
-                Console.WriteLine("Enter 1st number:");
-                a = int.Parse(Console.ReadLine()!);
-                Console.WriteLine("Enter 2nd number:");
-                b = int.Parse(Console.ReadLine()!);
-                result = MUL(a, b);
-                Console.WriteLine($"The product of {a} and {b} is {result}.");
-                break;
-
-            case 4:
-                Console.WriteLine("You selected Division.");
-                Console.WriteLine("Enter 1st number:");
-                a = int.Parse(Console.ReadLine()!);
-                Console.WriteLine("Enter 2nd number:");
-                b = int.Parse(Console.ReadLine()!);
-                result = DIV(a, b);
-                Console.WriteLine($"The result of dividing {a} by {b} is {result}.");
-                break;
-
-            case 5:
-                Console.WriteLine("You selected Modulus.");
-                Console.WriteLine("Enter 1st number:");
-                a = int.Parse(Console.ReadLine()!);
-                Console.WriteLine("Enter 2nd number:");
-                b = int.Parse(Console.ReadLine()!);
-                result = MOD(a, b);
-                Console.WriteLine($"The modulus of {a} % {b} is {result}.");
-                break;
-
-            default:
-                Console.WriteLine("Invalid selection. Please enter a number from 1 to 5.");
-                break;
-        }
     }
 }
